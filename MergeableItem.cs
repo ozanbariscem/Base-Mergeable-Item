df876c790c97ec037c531c7e55ea0215788e3bc1
ozanbariscem <ozanbarscem@gmail.com>
2020-08-24 17:04:48 +0300

    Base MergeableItem with test

---
+++ MergeableItem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+public class MergeableItem
+{
+    public int level;
+
+    /// <summary>
+    /// Checks for mergeable items in given list.
+    /// If it can find three mergeable items, merges them.
+    /// </summary>
+    /// <param name="items"></param>
+    public static void CheckMerge(List<MergeableItem> items) 
+    {
+        for (int i = items.Count - 1; i >= 0; i--)
+        {
+            List<MergeableItem> mergeableItems = new List<MergeableItem>() { items[i] };
+            for (int j = i - 1; j >= 0 && mergeableItems.Count != 3; j--)
+            {
+                if (items[i].IsMergeableWith(items[j]))
+                {
+                    mergeableItems.Add(items[j]);
+                }
+            }
+
+            if (mergeableItems.Count == 3)
+            {
+                Merge(items, mergeableItems);
+                i--;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Checks if this item is mergeable with given item.
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns>True if mergeable.</returns>
+    public bool IsMergeableWith(MergeableItem item) 
+    {
+        return this.level == item.level;
+    }
+
+    /// <summary>
+    /// Merges mergeable items into one.
+    /// Removes the excessive ones from the list.
+    /// </summary>
+    /// <param name="items">Complete list of items.</param>
+    /// <param name="itemsToMergeWith">Three of mergeable items.</param>
+    private static void Merge(List<MergeableItem> items, List<MergeableItem> itemsToMergeWith) 
+    {
+        itemsToMergeWith[0].level++;
+        for (int i = 1; i < itemsToMergeWith.Count; i++)
+            items.Remove(itemsToMergeWith[i]);
+    }
+}
---
+++ MergeableItemTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeableItemTest : MonoBehaviour
+{
+    private int totalCreated;
+    private int onPower;
+    public float createRate;
+    private float lastCreate;
+
+    List<MergeableItem> items;
+
+    void Start()
+    {
+        items = new List<MergeableItem>();
+        totalCreated = 0;
+        onPower = 0;
+    }
+
+    void Update()
+    {
+        if (Time.time - lastCreate >= createRate)
+        {
+            Create();
+
+            if (IsPowerOfThree(totalCreated))
+            {
+                Test();
+                onPower++;
+            }
+        }
+    }
+
+    void Create()
+    {
+        lastCreate = Time.time;
+        MergeableItem item = new MergeableItem();
+        items.Add(item);
+        MergeableItem.CheckMerge(items);
+        totalCreated++;
+    }
+
+    void Test()
+    {
+        bool countTest = CountTest();
+        bool levelTest = LevelTest();
+
+        Debug.Log(
+            string.Format("Testing:\nTotal created: {0}\nOn power: {1}\nCount test: {2}\nLevel test: {3}",
+            totalCreated, onPower, countTest, levelTest));
+
+        if (!countTest || !levelTest)
+        {
+            Debug.LogWarning("Failed test!");
+            Debug.Log(string.Format("Item count is: {0}, expected to be 1, state={1}", items.Count, items.Count == 1));
+            Debug.Log(string.Format("Item level is: {0}, expected to be {1}, state={2}", items[0].level, onPower, items[0].level == onPower));
+        }
+    }
+
+    bool CountTest()
+    {
+        return items.Count == 1;
+    }
+
+    bool LevelTest()
+    {
+        return items[0].level == onPower;
+    }
+
+    public bool IsPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+}
